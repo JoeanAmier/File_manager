7b45b0ea04e958013d3f5c658e4c0c034661f45c
youyq <yonglelolu@gmail.com>
2022-05-22 19:52:09 +0800

    Update

--- Rename.py
+++ Rename.py
@@ -8,3 +8,7 @@ class FileRename:
     @staticmethod
     def check_root(root):
         return os.path.exists(root) and os.path.isdir(root)
+
+    @staticmethod
+    def check_file(root):
+        return os.path.exists(root) and os.path.isfile(root)
